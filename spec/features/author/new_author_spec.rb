4f62e1f84d462234e63eccf78cabf57b318c9e51
Aaron Schlitt <aaron@aaronschlitt.de>
2022-10-26 16:13:17 +0200

    Implement add feature

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
+first_name = "Alan"
+last_name = "Turing"
+homepage = "https://wikipedia.org/Alan_Turing"
+full_name = "Alan Turing"
+
 describe 'New author page', type: :feature do
   it "should exist at 'new_author_path' and render withour error" do
     # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
@@ -12,13 +17,18 @@ describe 'New author page', type: :feature do
     expect(page).to(have_field('author[last_name]'))
     expect(page).to(have_field('author[homepage]'))
   end
+
+  it "should save the author when pressing submit" do
+    visit new_author_path
+    page.fill_in 'author[last_name]', with: last_name
+    page.fill_in 'author[first_name]', with: first_name
+    find('input[type="submit"]').click
+  end
+
 end
 
 describe "Author", type: :model do
-  first_name = "Alan"
-  last_name = "Turing"
-  homepage = "https://wikipedia.org/Alan_Turing"
-  full_name = "Alan Turing"
+
   it "should have first name, last name, and homepage" do
     author = Author.new(first_name: first_name, last_name: last_name, homepage: homepage)
     expect(author.first_name).to eq (first_name)
