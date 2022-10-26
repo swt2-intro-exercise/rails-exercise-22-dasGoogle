b526b36dbcea84dc367b1388757d55586bfe575b
Aaron Schlitt <aaron@aaronschlitt.de>
2022-10-26 14:51:24 +0200

    Add Author model

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -13,3 +13,21 @@ describe 'New author page', type: :feature do
     expect(page).to(have_field('author[homepage]'))
   end
 end
+
+describe "Author", type: :model do
+  first_name = "Alan"
+  last_name = "Turing"
+  homepage = "https://wikipedia.org/Alan_Turing"
+  full_name = "Alan Turing"
+  it "should have first name, last name, and homepage" do
+    author = Author.new(first_name: first_name, last_name: last_name, homepage: homepage)
+    expect(author.first_name).to eq (first_name)
+    expect(author.last_name).to eq (last_name)
+    expect(author.homepage).to eq (homepage)
+  end
+
+  it "should return the full name" do
+    author = Author.new(first_name: first_name, last_name: last_name, homepage: homepage)
+    expect(author.name).to eq (full_name)
+  end
+end
