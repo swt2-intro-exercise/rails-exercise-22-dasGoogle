36eb062b018bb3ab1ce002135547d314b19bb423
Aaron Schlitt <aaron@aaronschlitt.de>
2022-10-31 09:29:38 +0100

    Change class to correct one

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -17,7 +17,7 @@ class AuthorsController < ApplicationController
   end
 
   def update
-    @author = Article.find(params[:id])
+    @author = Author.find(params[:id])
 
     if @author.update(author_params)
       redirect_to @author
